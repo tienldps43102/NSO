e0bb9f62703f4b6211af6920f9ad5c3b9502d20d
tienldps43102 <tienldps43102@gmail.com>
2025-12-26 09:27:19 +0000

    desgin màu, font, spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,6 +51,9 @@ export default function Home() {
             />
             Deploy Now
           </a>
+          <Button >
+            Get Started
+          </Button>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
